8c46fb3e3f99cc93b4a8ccc5e7bcc3fdcedd0fd3
Jenny Qian <isjia@163.com>
2018-07-07 02:37:16 +0800

    fix scroll

--- src/base/slider/slider.vue
+++ src/base/slider/slider.vue
@@ -12,9 +12,10 @@
 
 <script type="text/ecmascript-6">
 import BScroll from 'better-scroll'
-import {addClass} from '@/common/js/dom'
+import { addClass } from '@/common/js/dom'
 
 export default {
+  name: 'slider',
   props: {
     loop: {
       type: Boolean,
@@ -50,12 +51,11 @@ export default {
         addClass(child, 'slider-item')
         child.style.width = sliderWidth + 'px'
         width += sliderWidth
-
-        if (this.loop) {
-          width += 2 * sliderWidth
-        }
-        this.$refs.sliderGroup.style.width = width + 'px'
       }
+      if (this.loop) {
+        width += 2 * sliderWidth
+      }
+      this.$refs.sliderGroup.style.width = width + 'px'
     },
     _initSlider () {
       this.slider = new BScroll(this.$refs.slider, {
@@ -73,42 +73,58 @@ export default {
 </script>
 
 <style scoped lang="stylus" rel="stylesheet/stylus">
-  @import "~@/common/stylus/variable"
-  .slider
-    min-height: 1px
-    .slider-group
-      position: relative
-      overflow: hidden
-      white-space: nowrap
-      .slider-item
-        float: left
-        box-sizing: border-box
-        overflow: hidden
-        text-align: center
-        a
-          display: block
-          width: 100%
-          overflow: hidden
-          text-decoration: none
-        img
-          display: block
-          width: 100%
-    .dots
-      position: absolute
-      right: 0
-      left: 0
-      bottom: 12px
-      text-align: center
-      font-size: 0
-      .dot
-        display: inline-block
-        margin: 0 4px
-        width: 8px
-        height: 8px
-        border-radius: 50%
-        background: $color-text-l
-        &.active
-          width: 20px
-          border-radius: 5px
-          background: $color-text-ll
+@import '~@/common/stylus/variable'
+
+.slider {
+  min-height: 1px;
+
+  .slider-group {
+    position: relative;
+    overflow: hidden;
+    white-space: nowrap;
+
+    .slider-item {
+      float: left;
+      box-sizing: border-box;
+      overflow: hidden;
+      text-align: center;
+
+      a {
+        display: block;
+        width: 100%;
+        overflow: hidden;
+        text-decoration: none;
+      }
+
+      img {
+        display: block;
+        width: 100%;
+      }
+    }
+  }
+
+  .dots {
+    position: absolute;
+    right: 0;
+    left: 0;
+    bottom: 12px;
+    text-align: center;
+    font-size: 0;
+
+    .dot {
+      display: inline-block;
+      margin: 0 4px;
+      width: 8px;
+      height: 8px;
+      border-radius: 50%;
+      background: $color-text-l;
+
+      &.active {
+        width: 20px;
+        border-radius: 5px;
+        background: $color-text-ll;
+      }
+    }
+  }
+}
 </style>
