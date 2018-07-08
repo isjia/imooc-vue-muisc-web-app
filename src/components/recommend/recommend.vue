e197e382c38f40dc039b8f2ff8c9ab266f6f453d
Jenny Qian <isjia@163.com>
2018-07-08 23:57:24 +0800

    done recommend page

--- src/components/recommend/recommend.vue
+++ src/components/recommend/recommend.vue
@@ -1,20 +1,39 @@
 <template>
-  <div class="recommend">
-    <div class="recommend-content">
+  <div class="recommend" ref="recommend">
+    <scroll class="recommend-content" :data="discList" ref="scroll">
+      <div>
       <div v-if="recommends.length"  class="slider-wrapper">
-        <slider>
-          <div v-for="item in recommends" :key="item.id">
-            <a :href="item.linkUrl">
-              <img :src="item.picUrl" alt="item.id">
-            </a>
-          </div>
-        </slider>
+        <div class="slider-content">
+          <slider ref="slider">
+            <div v-for="item in recommends" :key="item.id">
+              <a :href="item.linkUrl">
+                <img class="needsclick" @load="loadImage" :src="item.picUrl" alt="item.id">
+              </a>
+            </div>
+          </slider>
+        </div>
       </div>
       <div class="recommend-list">
-        <h1 class="list-title">热门推荐</h1>
-        <ul></ul>
+        <h1 class="list-title">热门歌单推荐</h1>
+        <ul>
+          <li class="item" v-for="item in discList" :key="item.dissid">
+            <div class="icon">
+              <img v-lazy="item.imgurl" width="60px" height="60px" alt="">
+            </div>
+            <div class="text">
+              <h2 class="name" v-html="item.creator.name"></h2>
+              <p class="desc" v-html="item.dissname"></p>
+            </div>
+          </li>
+        </ul>
       </div>
-    </div>
+      </div>
+
+      <!-- Loading -->
+      <div class="loading-container" v-show="!discList.length">
+        <loading></loading>
+      </div>
+    </scroll>
   </div>
 </template>
 
@@ -22,15 +41,20 @@
 import {getRecommend, getDiscList} from '@/api/recommend'
 import {ERR_OK} from '@/api/config'
 import Slider from '@/base/slider/slider'
+import Scroll from '@/base/scroll/scroll'
+import Loading from '@/base/loading/loading'
 
 export default {
   data () {
     return {
-      recommends: []
+      recommends: [],
+      discList: []
     }
   },
   components: {
-    Slider
+    Slider,
+    Scroll,
+    Loading
   },
   created () {
     // 获取数据
@@ -49,9 +73,15 @@ export default {
     _getDiscList () {
       getDiscList().then((res) => {
         if (res.code === ERR_OK) {
-          console.log(res.data.list)
+          this.discList = res.data.list
         }
       })
+    },
+    loadImage () {
+      if (!this.checkLoaded) {
+        this.$refs.scroll.refresh()
+        this.checkLoaded = true
+      }
     }
   }
 }
