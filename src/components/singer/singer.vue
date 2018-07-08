a46d359d7d2e7690ac0fef2f7c69c0181f18b216
Jenny Qian <isjia@163.com>
2018-07-09 01:54:50 +0800

    init singer page

--- src/components/singer/singer.vue
+++ src/components/singer/singer.vue
@@ -1,9 +1,91 @@
 <template>
-  <div>歌手页面</div>
+  <div class="singer">
+    <list-view :data="singers">
+
+    </list-view>
+  </div>
 </template>
 
 <script type="text/ecmascript-6">
+import {getSingerList} from '@/api/singer'
+import {ERR_OK} from '@/api/config'
+import Singer from '@/common/js/singer'
+import ListView from '@/base/listview/listview'
 
+const HOT_NAME = '热门歌手'
+const HOT_SINGER_LENGTH = 10
+
+export default {
+  components: {
+    ListView
+  },
+  data () {
+    return {
+      singers: []
+    }
+  },
+  created () {
+    this._getSingerList()
+  },
+  methods: {
+    _getSingerList () {
+      getSingerList().then((res) => {
+        if (res.code === ERR_OK) {
+          this.singers = this._normalizeSinger(res.data.list)
+        }
+      })
+    },
+    _normalizeSinger (list) {
+      let map = {
+        hot: {
+          title: HOT_NAME,
+          items: []
+        }
+      }
+      list.forEach((item, index) => {
+        if (index < HOT_SINGER_LENGTH) {
+          map.hot.items.push(new Singer({
+            id: item.Fsinger_mid,
+            name: item.Fsinger_name
+          }))
+        }
+        const key = item.Findex
+        if (!map[key]) {
+          map[key] = {
+            title: key,
+            items: []
+          }
+        }
+        map[key].items.push(new Singer({
+          id: item.Fsinger_mid,
+          name: item.Fsinger_name
+        }))
+      })
+      // 排序
+      let hot = []
+      let ret = []
+      for (let key in map) {
+        let val = map[key]
+
+        if (val.title.match(/[a-zA-Z]/)) {
+          ret.push(val)
+        } else if (val.title === HOT_NAME) {
+          hot.push(val)
+        }
+      }
+      ret.sort((a, b) => {
+        return a.title.charCodeAt(0) - b.title.charCodeAt(0)
+      })
+      return hot.concat(ret)
+    }
+  }
+}
 </script>
 
-<style scoped lang="stylus" rel="stylesheet/stylus"></style>
+<style scoped lang="stylus" rel="stylesheet/stylus">
+.singer
+  position: fixed
+  top: 88px;
+  bottom: 0
+  width: 100%
+</style>
